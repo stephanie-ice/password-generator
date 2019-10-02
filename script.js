c1b99985eaa968d81bdd793d6489d762eb2b6ca5
Stephanie Ice <stephanie.icee@gmail.com>
2019-10-02 13:39:50 -0700

    moved js logic to script.js and coded HTML outline

---
+++ script.js
@@ -0,0 +1,27 @@
+<script type="text/JavaScript">
+    
+       for (var i=0; i<12; i++) {
+        
+        var lowAlpha = ["a","b","c","d","e","f","g","h","i","j"]
+
+        var uppAlpha = ["A","B","C","D","E","F","G","H","I","J"];
+
+        var number = [0,1,2,3,4,5,6,7,8,9];
+
+        var specChar = ["!","@","#","$","%","^"];
+
+        var allArrs = [lowAlpha,uppAlpha,number,specChar];
+
+        console.log (allArrs[3]);
+        
+        
+        var arrs = allArrs[Math.floor(Math.random()*allArrs.length)];
+
+        var digit = arrs[Math.floor(Math.random()* arrs.length)];
+        
+        console.log (digit);
+        
+        }
+        
+
+    </script>
