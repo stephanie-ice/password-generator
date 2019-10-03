c4d51ed3c9bcff77f720bb99174802e23139e3d5
Stephanie Ice <stephanie.icee@gmail.com>
2019-10-03 11:20:30 -0700

    tutoring help; added function for generating the password, and the click event

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
+function generate ()  {
+
+    var lowAlpha = ["a","b","c","d","e","f","g","h","i","j"];
+    
+    var uppAlpha = ["A","B","C","D","E","F","G","H","I","J"];
+    
+    var number = [0,1,2,3,4,5,6,7,8,9];
+    
+    var specChar = ["!","@","#","$","%","^"];
+    
+    var allArrs = [lowAlpha,uppAlpha,number,specChar];
+    
+    var password = "";
+    
+    for (var i=0; i<128; i++) {
+        
+        var arrs = allArrs[Math.floor(Math.random()*allArrs.length)];
+    
+        var digit = arrs[Math.floor(Math.random()* arrs.length)];
+        
+        password += digit;
+    
+    }
+    
+    console.log ("password: ", password);
+
+    return password
+}
        
-        var lowAlpha = ["a","b","c","d","e","f","g","h","i","j"];
-
-        var uppAlpha = ["A","B","C","D","E","F","G","H","I","J"];
-
-        var number = [0,1,2,3,4,5,6,7,8,9];
-
-        var specChar = ["!","@","#","$","%","^"];
-
-        var allArrs = [lowAlpha,uppAlpha,number,specChar];
-        
-        for (var i=0; i<12; i++) {
-            
-            var arrs = allArrs[Math.floor(Math.random()*allArrs.length)];
-
-            var digit = arrs[Math.floor(Math.random()* arrs.length)];
-            
-            console.log (digit);
-        
-        }
-        
+document.getElementById("generate").addEventListener("click", function(){
+    var password = generate ()
+    document.getElementById("password").value = password
+    document.getElementById("generate").textContent = "example"
+});
